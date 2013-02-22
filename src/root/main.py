036135f95f03b267ece8ceff7ef12daed2f203bb
RichyH <github@blacksun.at>
2013-02-22 01:34:09 +0100

    Nightly 20130221

--- src/root/main.py
+++ src/root/main.py
@@ -5,4 +5,14 @@ Created on 21.02.2013
 '''
 
 if __name__ == '__main__':
-    print "Der Juergen ist ur super!"
+    
+    def druck(l,s):
+        if l > 0:
+            print l,'x',s
+            return druck(l-1,s*l)
+        else:
+            return 0,'x',s
+    
+    rest = druck(4,"Mauzi")
+    print rest[0],rest[1],rest[2]
+    
