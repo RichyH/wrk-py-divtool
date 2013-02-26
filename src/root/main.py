7da1bce6960abf536db461e547808dd040b5b2af
RichyH <github@blacksun.at>
2013-02-26 23:04:22 +0100

    20130226

--- src/root/main.py
+++ src/root/main.py
@@ -19,28 +19,26 @@ def leseDatei(datei):
 
 if __name__ == '__main__':
     regSplit = re.compile(r'\t|\n')
-    regSearch = re.compile(r'\A.*',re.I | re.M)
+    regSearchV4DIR = re.compile(r'(^DIR).*\n(^\d\d[0-9 ]{3}).{48}([-=\w(,) ]*)[A-Z0-9:-=\s]*^[ ]{64}([-=\w(,) ]*)',re.I | re.M)
+    regSearchV5DIR = re.compile(r'(^DIRECTORY NUMBER):.([0-9]{1,5}).*\n(^DIV STATE):.([A-Z, ]{1,}).*\n(^ADD INFO):.([-=\w(,) ]*)',re.I | re.M)
     items = []
     
 #    print leseDatei("../../SUSIP_test.txt")
     datei = open("../../SUSIP_test.txt", "r")
-        
-#    for zeile in datei:
-#        items.append(re.split(regSplit,zeile))
-#    for item in items:
-#        print item
     rawdata = datei.read()
     datei.close()
+    del datei
 
-    m = regSearch.search(rawdata)
-    print m.expand()
+    print 'V4:\n'
+    result = regSearchV4DIR.finditer(rawdata)
+    for dirs in result:
+        print dirs.group(1) + ': [' + dirs.group(2) + '] [' + dirs.group(3) + '] [' + dirs.group(4) + ']'
+    print '\nV5:\n'
+    result = regSearchV5DIR.finditer(rawdata)
+    for dirs in result:
+        print dirs.group(1) + ': [' + dirs.group(2) + ']\t' + dirs.group(3) + ': [' + dirs.group(4) + ']\t' + dirs.group(5) + ': [' + dirs.group(6) + ']'
     print 'Ende'
 #    del datei, zeile
-
-
-
-
-
 
 
 '''
